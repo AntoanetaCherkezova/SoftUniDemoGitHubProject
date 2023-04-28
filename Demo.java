6e19373609243acf40990c6121e5d1047d10255a
AntoanetaCherkezova <132072392+AntoanetaCherkezova@users.noreply.github.com>
2023-04-28 17:01:42 +0300

    Added code in demo

--- Demo.java
+++ Demo.java
@@ -1,2 +1,7 @@
 public class Demo {
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    int number = Integer.parseInt(scanner.nextLine());
+    System.out.println(number);
+  }
 }
